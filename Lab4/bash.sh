d465c446719c7bc1e89e5c30a1d00caed7d5b3b8
SvetlanaGolub <golub.sveta@yandex.ru>
2019-12-18 19:16:54 +0300

    2.3 - вывод из файла bash.txt

---
+++ Lab4/bash.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+grep 000000 ~/bash.txt >> /tmp/zeros
+grep -v 000000 ~/bash.txt >> /tmp/nozeros
+head -n10 /tmp/zeros
+tail -n10 /tmp/zeros
+head -n10 /tmp/nozeros
+tail -n10 /tmp/nozeros
